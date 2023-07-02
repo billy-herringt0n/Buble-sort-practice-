42efcbb43a34a8ec530763a80dd196f3abc82510
Aleks <Aleksnikmaster@gmail.com>
2023-07-02 22:54:33 +0300

    Доработка кода

--- Project1/main.c
+++ Project1/main.c
@@ -7,6 +7,7 @@
 #include <time.h>
 
 int size, choice;
+int* arr = 0;
 char n_file[49];
 FILE* file;
 
@@ -41,11 +42,32 @@ void writeArrayToFile(int arr[], int size) {
         fprintf(file, "%d ", arr[i]);
 
     fclose(file);
-    printf("Данные успешно записаны!\n");;
+    printf("Данные успешно записаны!\n");
+}
+void readArrayFromFile() {
+    printf("Введите имя файла для загрузки данных из него: ");
+    scanf("%s", n_file);
+    if (sizeof(n_file) > 49) {
+        printf("Имя файла превышает доступное значение (49)!");
+        return;
+    }
+    if ((file = fopen(n_file, "r+")) == 0)
+    {
+        printf("Невозможно открыть файл для чтения %s\n", n_file);
+        _getch();
+        return;
+    }
+    fscanf(file, "%d", &size);
+    arr = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        fscanf(file, "%d", &arr[i]);
+    }
+    fclose(file);
+    printf("Данные успешно прочитаны!\n");
 }
 
 void helloScreen() {
-    printf("Практическая работа\nНа тему: \"Сортировка пузырьком (bubble sort)\"\n\nВыполнили студенты группы 22ВВВ1: Курушин Ярослав, Николаев Александр, Ганин Иван.\n\n");
+    printf("\tПрактическая работа\nНа тему: \"Сортировка пузырьком (bubble sort)\"\n\nВыполнили студенты группы 22ВВВ1: Курушин Ярослав, Николаев Александр, Ганин Иван.\n\n");
     system("PAUSE");
 }
 
@@ -56,34 +78,23 @@ void main() {
     clock_t start, end;
     double time_spent = 0.0;
     system("cls");
-    printf("Для начала нужно создать массив\n");
-    printf("Введите размер массива: ");
-    scanf("%d", &size);
-
-    int* arr = (int*)malloc(size * sizeof(int));
-    if (arr == NULL) {
-        printf("Не удалось выделить память!\n");
-        return;
-    }
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand();
-    }
-    printf("Массив создан\n");
-    
     do {
+        printf("\tМеню\n");
         printf("1. Просмотреть массив\n");
         printf("2. Отсортировать массив\n");
         printf("3. Записать массив в файл\n");
-        printf("4. Посмотреть время выполнения алгоритма\n");
-        printf("5. Выход\n");
+        printf("4. Загрузить массив из файла\n");
+        printf("5. Посмотреть время выполнения алгоритма\n");
+        printf("6. Создать новый массив\n");
+        printf("7. Выход\n");
         printf("Выберите действие: ");
-
         scanf("%d", &choice);
         switch (choice) {
         case 1: //Просмотр
-            printf("Массив: ");
+            system("cls");
+            printf("Массив:\n ");
             for (int i = 0; i < size; i++) {
-                printf("%d ", arr[i]);
+                printf("%d) %d\n ", i+1, arr[i]);
             }
             printf("\n");
             break;
@@ -100,7 +111,15 @@ void main() {
             system("cls");
             writeArrayToFile(arr, size);
             break;
-        case 4: // Время
+        case 4: // Запись в файл
+            system("cls");
+            if (arr != NULL) 
+            {
+                free(arr);
+            }
+            readArrayFromFile();
+            break;
+        case 5: // Время
             system("cls");
             if (time_spent == 0) {
                 printf("Сначала отсортируйте массив!\n");
@@ -108,17 +127,30 @@ void main() {
             }
              printf("Время выполнения алгоритма: %f\n", time_spent);
              break;
-        case 5: // Выход
+        case 6: //Ввод данных 
+            system("cls");
+            free(arr);
+            printf("Введите размер массива: ");
+            scanf("%d", &size);
+            arr = (int*)malloc(size * sizeof(int));
+            if (arr == NULL) {
+                printf("Не удалось выделить память!\n");
+                return;
+            }
+            for (int i = 0; i < size; i++) {
+                arr[i] = rand();
+            }
+            break;
+        case 7: // Выход
              system("cls");
              printf("До свидания!\n");
-             //free(arr);
              break;
         default:
              system("cls");
              printf("Некорректный выбор!\n");
              break;
         }
-    } while (choice != 5);
+    } while (choice != 7);
 
     getchar();
 }
